a3f1144fcdfe5b1a5b2cf7c0c72fa6dfcb43b569
Galiev Ramil <galievi.f@yandex.ru>
2022-10-24 10:27:04 +0300

    burger_menu

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 let menu = document.querySelector(".menu");
 
+console.log(menu.firstElementChild.offsetHeight);
 
 const menuHandler = (e) => {
 	if (!e.target.closest("div.header__burger")) {
-		menu.classList.remove("active");        
+		// menu.classList.remove("active");
+		menu.style.maxHeight = "0px";
 		return;
 	}
-	menu.classList.add("active");   
+	// menu.classList.add("active");
+	menu.style.maxHeight = menu.firstElementChild.offsetHeight + "px";
 };
 
 document.body.addEventListener("click", menuHandler);
